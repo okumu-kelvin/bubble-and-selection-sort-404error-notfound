27d8d8c30ca6b7b0c53f7828ed3406706f0350e6
Angela Omondi <omondiangela426@gmail.com>
2025-06-20 10:00:37 +0300

    Update selection_sort.py
    Selection sort algorithm

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,11 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    for i in range(0, len(arr)-1):
+        current_minimum = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[current_minimum]:
+                current_minimum = j
+
+        arr[i], arr[current_minimum] = arr[current_minimum], arr[i]
+    return arr
+arr = [36,24,7,18,11,30,50,2]
+print("Sorted List: ", selection_sort(arr))
